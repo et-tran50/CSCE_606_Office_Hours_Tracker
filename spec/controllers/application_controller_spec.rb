dcc03328408320a0b9e48027c777aaf950596e2a
brysonBrown <bryson.brown@tamu.edu>
2024-10-02 13:10:28 -0500

    just added the code to set up rspec tests

---
+++ spec/controllers/application_controller_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe ApplicationController, type: :controller do
+
+  describe 'when trying to find movies by the same director' do
+    it 'returns a valid collection when a valid director is present' do
+      
+    end
+  end
+end
