1327ada84033a4d259c320958ee897972f1f7dfa
gourangi0309 <gourangitaware@tamu.edu>
2024-10-18 17:15:36 -0500

    TA Attendance edge case updated

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -13,9 +13,14 @@ class AttendancesController < ApplicationController
 
     if user
       if ta_email?(email)  # Check if the user is a TA using the same logic as in SessionsController
-        TaAttendance.create(user_id: user.id, sign_in_time: Time.now)
-        session[:attendance_marked] = true
-        # flash[:notice] = "TA attendance marked successfully!"
+        if attendance_marked_recently?(user)
+          flash[:notice] = "Attendance already marked for the time slot"
+        else
+          # Mark attendance if no recent entry found
+          TaAttendance.create(user_id: user.id, sign_in_time: Time.now, course_id: "102-PT")
+          session[:attendance_marked] = true
+          flash[:notice] = "TA attendance marked successfully!"
+        end
       else
         Attendance.create(user_id: user.id, sign_in_time: Time.now, course_id: course_number)
         session[:stu_attendance_marked] = true
@@ -32,6 +37,11 @@ class AttendancesController < ApplicationController
   end
 
   private
+
+  def attendance_marked_recently?(user)
+    # Check if there is a recent attendance record for the user within the last hour
+    TaAttendance.where(user_id: user.id, sign_in_time: (Time.now - 1.hour)..Time.now).exists?
+  end
 
   def determine_redirect_path(email, course_number)
     if ta_email?(email)
