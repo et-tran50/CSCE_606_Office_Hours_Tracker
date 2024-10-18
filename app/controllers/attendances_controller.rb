684a66eb0f845bdef9438502eab02b81ce395c13
gourangi0309 <gourangitaware@tamu.edu>
2024-10-18 17:48:16 -0500

    Student attendance edge case updated

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -13,7 +13,7 @@ class AttendancesController < ApplicationController
 
     if user
       if ta_email?(email)  # Check if the user is a TA using the same logic as in SessionsController
-        if attendance_marked_recently?(user)
+        if attendance_marked_recently?(user, "ta")
           flash[:notice] = "Attendance already marked for the time slot"
         else
           # Mark attendance if no recent entry found
@@ -22,9 +22,14 @@ class AttendancesController < ApplicationController
           flash[:notice] = "TA attendance marked successfully!"
         end
       else
-        Attendance.create(user_id: user.id, sign_in_time: Time.now, course_id: course_number)
-        session[:stu_attendance_marked] = true
-        # flash[:notice] = "Attendance marked successfully!"
+        if attendance_marked_recently?(user, "student") # Check for student attendance
+          flash[:notice] = "Attendance already marked for the time slot"
+        else
+          # Mark attendance for Student if no recent entry found
+          Attendance.create(user_id: user.id, sign_in_time: Time.now, course_id: course_number)
+          session[:stu_attendance_marked] = true
+          flash[:notice] = "Student attendance marked successfully!"
+        end
       end
 
       # Rails.logger.info "Attendance marked: #{session[:attendance_marked]}"  # Log to verify
@@ -38,9 +43,14 @@ class AttendancesController < ApplicationController
 
   private
 
-  def attendance_marked_recently?(user)
-    # Check if there is a recent attendance record for the user within the last hour
-    TaAttendance.where(user_id: user.id, sign_in_time: (Time.now - 1.hour)..Time.now).exists?
+  def attendance_marked_recently?(user, role)
+    if role == "ta"
+      # Check TA attendance within the last hour
+      TaAttendance.where(user_id: user.id, sign_in_time: (Time.now - 1.hour)..Time.now).exists?
+    else
+      # Check Student attendance within the last hour
+      Attendance.where(user_id: user.id, sign_in_time: (Time.now - 1.hour)..Time.now).exists?
+    end
   end
 
   def determine_redirect_path(email, course_number)
